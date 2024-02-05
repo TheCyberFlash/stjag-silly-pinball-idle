ded189e7ae9037df0debf3bcaa0c2953f0c147fd
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-05 19:01:56 +0100

    Update game.js

--- silly-pinball/js/game.js
+++ silly-pinball/js/game.js
@@ -2,6 +2,13 @@ class Game {
   constructor(canvas, ctx) {
     this.canvas = canvas;
     this.ctx = ctx;
+    this.ball = {
+        x: this.canvas.width / 2,
+        y: this.canvas.height - 30,
+        radius: 10,
+        dx: 2,
+        dy: -2,
+    };
   }
 
   start() {
