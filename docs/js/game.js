3f3f06634a1077341655225f49f0c7b4d2988a21
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-05 19:16:48 +0100

    Update game.js

--- docs/js/game.js
+++ docs/js/game.js
@@ -9,6 +9,22 @@ class Game {
       dx: 2,
       dy: -2,
     };
+    this.paddle = {
+      width: 100,
+      height: 10,
+      x: this.canvas.width / 2 - 50,
+      y: this.canvas.height - 20,
+      speed: 5,
+    };
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        this.paddle.x += this.paddle.speed;
+      }
+      if (e.key === "ArrowLeft") {
+        this.paddle.x -= this.paddle.speed;
+      }
+    });
   }
 
   start() {
@@ -19,29 +35,70 @@ class Game {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.drawBall();
+    this.drawPaddle();
   }
 
   drawBall() {
     this.ctx.beginPath();
     this.ctx.arc(this.ball.x, this.ball.y, this.ball.radius, 0, Math.PI * 2);
-    this.ctx.fillStyle = '#0095DD';
+    this.ctx.fillStyle = "#0095DD";
     this.ctx.fill();
     this.ctx.closePath();
   }
 
-    update() {
-        this.ball.x += this.ball.dx;
-        this.ball.y += this.ball.dy;
-    
-        if (this.ball.x + this.ball.dx > this.canvas.width - this.ball.radius || this.ball.x + this.ball.dx < this.ball.radius) {
-        this.ball.dx = -this.ball.dx;
-        }
-    
-        if (this.ball.y + this.ball.dy > this.canvas.height - this.ball.radius || this.ball.y + this.ball.dy < this.ball.radius) {
-        this.ball.dy = -this.ball.dy;
-        }
-    
-        this.draw();
-        requestAnimationFrame(this.update.bind(this));
+  drawPaddle() {
+    this.ctx.beginPath();
+    this.ctx.rect(
+      this.paddle.x,
+      this.paddle.y,
+      this.paddle.width,
+      this.paddle.height
+    );
+    this.ctx.fillStyle = "#0095DD";
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
+  update() {
+    this.ball.x += this.ball.dx;
+    this.ball.y += this.ball.dy;
+
+    if (
+      this.ball.x + this.ball.dx > this.canvas.width - this.ball.radius ||
+      this.ball.x + this.ball.dx < this.ball.radius
+    ) {
+      this.ball.dx = -this.ball.dx;
     }
+
+    if (
+      this.ball.y + this.ball.dy > this.canvas.height - this.ball.radius ||
+      this.ball.y + this.ball.dy < this.ball.radius
+    ) {
+      this.ball.dy = -this.ball.dy;
+    }
+
+    if (
+      this.ball.y + this.ball.dy >
+        this.canvas.height - this.ball.radius - this.paddle.height &&
+      this.ball.x > this.paddle.x &&
+      this.ball.x < this.paddle.x + this.paddle.width
+    ) {
+      this.ball.dy = -this.ball.dy;
+    }
+
+    this.draw();
+    requestAnimationFrame(this.update.bind(this));
+  }
+
+  launchBall() {
+    if (this.ball.dy === 0) {
+      this.ball.dy = -2;
+    }
+  }
+
+  handleKeyPress(event) {
+    if (event.key === " " || event.key === "Spacebar") {
+      this.launchBall();
+    }
+  }
 }
