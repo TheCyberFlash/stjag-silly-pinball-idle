3f872e042b28b87c1a0fe84612e9a15cd57a4aaf
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-05 19:09:02 +0100

    new build

--- docs/js/game.js
+++ docs/js/game.js
@@ -2,10 +2,46 @@ class Game {
   constructor(canvas, ctx) {
     this.canvas = canvas;
     this.ctx = ctx;
+    this.ball = {
+      x: this.canvas.width / 2,
+      y: this.canvas.height - 30,
+      radius: 10,
+      dx: 2,
+      dy: -2,
+    };
   }
 
   start() {
-    this.ctx.fillStyle = "black";
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.draw();
+    this.update();
   }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.drawBall();
+  }
+
+  drawBall() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.ball.x, this.ball.y, this.ball.radius, 0, Math.PI * 2);
+    this.ctx.fillStyle = '#0095DD';
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
+    update() {
+        this.ball.x += this.ball.dx;
+        this.ball.y += this.ball.dy;
+    
+        if (this.ball.x + this.ball.dx > this.canvas.width - this.ball.radius || this.ball.x + this.ball.dx < this.ball.radius) {
+        this.ball.dx = -this.ball.dx;
+        }
+    
+        if (this.ball.y + this.ball.dy > this.canvas.height - this.ball.radius || this.ball.y + this.ball.dy < this.ball.radius) {
+        this.ball.dy = -this.ball.dy;
+        }
+    
+        this.draw();
+        requestAnimationFrame(this.update.bind(this));
+    }
 }
